279eb98de5f8235f912c6a99a26f26f56e4260c1
kerrystilling <109369691+kerrystilling@users.noreply.github.com>
2024-02-16 21:09:09 -0600

    testing

--- src/assets/javascript/animations/homepage.js
+++ src/assets/javascript/animations/homepage.js
@@ -1,5 +1,44 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 if(document.querySelector(`[data-page="home"]`)) {
-    gsap.from("[data-headline]", {autoAlpha: 0, y:128, duration:2, clearProps: "all"})
+
+    function heroScrolls() {
+      gsap.from("[data-headline]", {autoAlpha: 0, y:128, duration:2, clearProps: "all"})
+
+      let tl = gsap.timeline({
+          // yes, we can add it to an entire timeline!
+          scrollTrigger: {
+            trigger: "[data-hero]",
+            pin: true, // pin the trigger element while active
+            start: "top top", // when the top of the trigger hits the top of the viewport
+            end: "bottom top", // end after scrolling 500px beyond the start
+            scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+          },
+      });
+        
+        // add animations and labels to the timeline
+      tl
+        .to("[data-hero-morph]", { borderRadius: 320 })
+    }
+    heroScrolls();
+
+    function hoverImg () {
+      const trigger = document.querySelector('[data-hover-1="trigger"]');
+      const imgs = document.querySelector('[data-hover-1="img"]');
+
+      function showImgs() {
+        console.log('click')
+        let tl = gsap.timeline({});
+        tl
+          .to(imgs, {autoAlpha:1, duration:1})
+      }
+
+      trigger.addEventListener('click', showImgs, false)
+    }
+    hoverImg();
+
 }
