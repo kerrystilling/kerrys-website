1b37c63c5aecdaf32760a00c864e1a97bf6ec2e7
kerrystilling <109369691+kerrystilling@users.noreply.github.com>
2024-04-03 17:43:00 -0500

    bunch o stuff

--- src/assets/javascript/animations/homepage.js
+++ src/assets/javascript/animations/homepage.js
@@ -1,44 +1,76 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 gsap.registerPlugin(ScrollTrigger);
 
+//Hero Animation
+function heroAnimate () {
+  const duration = 0.5;
+  const offset = "=-0.25";
+  let tl = gsap.timeline({});
+    tl
+      .set(['[data-hero-shape-1]', '[data-hero-shape-2]', '[data-hero-shape-3]'], { opacity: 0, rotation: 0 }, 0 )
+      .set(['[data-hero-word-1]', '[data-hero-word-2]', '[data-hero-word-3]', '[data-hero-word-4]', '[data-hero-word-5]', '[data-hero-word-6]', '[data-hero-word-7]', '[data-hero-word-8]','[data-hero-word-9]'], { opacity: 0, rotation: 0 }, 0 )
+      .to('[data-hero-word-1]',  { opacity: 1, duration: duration }, duration)
+      .to('[data-hero-shape-1]', { opacity: 1, rotation: -45, duration: duration }, `${offset}`)
+      .to('[data-hero-word-2]',  { opacity: 1, duration: duration }, `${offset}`)
+      .to('[data-hero-word-3]',  { opacity: 1, duration: duration }, `${offset}`)
+      .to('[data-hero-word-4]',  { opacity: 1, duration: duration }, `${offset}`)
+      .to('[data-hero-word-5]',  { opacity: 1, duration: duration }, `${offset}`)
+      .to('[data-hero-shape-2]', { opacity: 1, rotation: 90, duration: duration }, `${offset}`)
+      .to('[data-hero-word-6]',  { opacity: 1, duration: duration }, `${offset}`)
+      .to('[data-hero-word-7]',  { opacity: 1, duration: duration }, `${offset}`)
+      .to('[data-hero-shape-3]', { opacity: 1, rotation: 90, duration: duration }, `${offset}`)
+      .to('[data-hero-word-8]',  { opacity: 1, duration: duration }, `${offset}`)
+      .to('[data-hero-word-9]',  { opacity: 1, duration: duration }, `${offset}`);
+}
+heroAnimate();
 
-if(document.querySelector(`[data-page="home"]`)) {
+//Section 2 Images on Scroll
+function section2Animate () {
+  const duration = 0;
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      scrub: 1,
+      trigger: '[data-img-trigger]',
+      start: "top bottom",
+      end: 'bottom top',
+    },
+  });
+  
+  tl
+    .to('[data-s2-img-1]', {x: -500, y: -400, rotation: -15}, '-=1')
+    .to('[data-s2-img-2]', {x: 200, y: 260}, '-=1')
+    .to('[data-s2-img-3]', {x: 380, y: -300, rotation: 5}, '-=1')
+    .to('[data-s2-img-4]', {x: -300, y: -260, rotation: 0}, '-=1')
+}
+section2Animate ();
 
-    function heroScrolls() {
-      gsap.from("[data-headline]", {autoAlpha: 0, y:128, duration:2, clearProps: "all"})
+// Hover images
+function hoverImg () {
+  const triggers = document.querySelectorAll('[data-hover-img');
 
-      let tl = gsap.timeline({
-          // yes, we can add it to an entire timeline!
-          scrollTrigger: {
-            trigger: "[data-hero]",
-            pin: true, // pin the trigger element while active
-            start: "top top", // when the top of the trigger hits the top of the viewport
-            end: "bottom top", // end after scrolling 500px beyond the start
-            scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-          },
-      });
-        
-        // add animations and labels to the timeline
+  triggers.forEach(trigger => {
+    const image = trigger.querySelector('[data-hover-img-el]',);
+
+    function showImgs() {
+      console.log('hover')
+      let tl = gsap.timeline({});
       tl
-        .to("[data-hero-morph]", { borderRadius: 320 })
+        .to(image, {autoAlpha: 1, duration: .25})
+        .to(image, {rotation: 0, duration: .5, ease: "elastic.out(1,0.5)"}, '=-0.25')
     }
-    heroScrolls();
-
-    function hoverImg () {
-      const trigger = document.querySelector('[data-hover-1="trigger"]');
-      const imgs = document.querySelector('[data-hover-1="img"]');
-
-      function showImgs() {
-        console.log('click')
-        let tl = gsap.timeline({});
-        tl
-          .to(imgs, {autoAlpha:1, duration:1})
-      }
-
-      trigger.addEventListener('click', showImgs, false)
+  
+    function hideImgs() {
+      console.log('mouseout')
+      let tl = gsap.timeline({});
+      tl
+        .to(image, {autoAlpha:0, duration:.125})
+        .to(image, {rotation: 15}, '=-0.125')
     }
-    hoverImg();
+  
+    trigger.addEventListener('mouseenter', showImgs, false)
+    trigger.addEventListener('mouseout', hideImgs, false)
+  })
 
 }
+hoverImg();
